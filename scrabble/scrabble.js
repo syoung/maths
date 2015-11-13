30a5f2faf855ddd8fd05274a92b325e0cd715599
Stuart Young <stuartpyoung@gmail.com>
2015-11-12 22:22:04 -0500

    -Inc scrabble/scrabble.js: Handles user input (six lines), todo - doScrabble method

--- scrabble/scrabble.js
+++ scrabble/scrabble.js
@@ -65,142 +65,39 @@ var prompt = reader.createInterface(process.stdin, process.stdout);
 var lingeringLine = "";
 var lines = new Array();
 
+/* DO SCRABBLE COUNTS */
 function doScrabble (inputArray) {
     console.log("doScrabble    inputArray", inputArray);
+    var score = 0;
+
+
+
 };
 
+
+/* GET USER DATA FROM COMMAND LINE PROMPT */
 prompt.
 on('line', function(line) {
     var msg = line.trim();
-    console.log("msg", msg);
-    
+    //console.log("msg", msg);
     lines.push(msg);
-    console.log("lines", lines);
-    console.log("lines.length", lines.length);
+    //console.log("lines", lines);
+    //console.log("lines.length", lines.length);
     
-    if (lines.length == 2) {
-        console.log("lines    doScrabble()");
+    if (lines.length == 6) {
+        //console.log("lines    doScrabble()");
         doScrabble(lines);
-    }
-    
+        prompt.close();
+    }    
 }).
 on('close', function() {
-    console.log("lines", lines);
-
-        
-
+    //console.log("prompt   close  lines", lines);
     process.exit(0);
 });
-
 prompt.setPrompt("> ", 2);
 prompt.prompt();
+
 
 console.log("COMPLETED");
 
 
-/*
-
-// 1. Pre-calculate all of the prime numbers under 1000
-function precalculatePrimes (maxNumber) {
-    // for each n until N > 1000
-    // N = n**2 + n + 41
-    var primeNumber = 1;
-    var startNumber = 1;
-    while ( primeNumber < maxNumber ) {
-        primeNumber = Math.pow(startNumber, 2) + startNumber + 41
-        //console.log("primeNumber: ", primeNumber);
-        boardArray.push(primeNumber);
-        startNumber = startNumber + 1;
-    }
-    var trash = boardArray.pop();
-    //console.log("trash", trash);
-    //console.log("boardArray", boardArray);
-};
-
-// 2. Go through all of the boxes finding their factors in terms of multiple of primes
-//    For each box number, divide by the lowest prime (2) as many times as possible, then
-//      divide by the next prime (3) as many times as possible and so on..
-//     N = prime1**a . prime2**b . prime3**c ... etc.
-function findBoxExponents () {
-    for ( var boxIndex = 0; boxIndex < 1000; boxIndex++ ) {
-        var boxNumber = boxIndex + 1;
-        console.log("boxNumber", boxNumber);
-        var exponents = calculateExponents(boxNumber);
-        console.log("Exponents for boxNumber " + boxNumber + ": ", exponents);
-        boxPrimeExponents.push(exponents);
-    }
-    console.log("boxPrimeExponents", boxPrimeExponents);
-}
-
-function calculateExponents (boxNumber) {
-    console.log("DOING calculateExponents for boxNumber: " + boxNumber);
-    var exponents = [];
-    for ( var primeIndex = 0; primeIndex < boardArray.length; primeIndex++ ) {
-        var primeNumber = boardArray[primeIndex];
-        console.log("primeNumber", primeNumber);
-        if ( boxNumber < primeNumber ) {
-            console.log("boxNumber " + boxNumber + " is less than primeNumber " + primeNumber);
-            return exponents;
-        }
-        
-        var remainder = boxNumber;
-        var storedResult = boxNumber;
-        var primeNumber = boardArray[primeIndex];
-        var exponent = 0;
-        remainder = remainder/primeNumber;
-        if ( cleanDivision(remainder) ) {
-            while ( cleanDivision(remainder) ) {            
-                exponent = exponent + 1;
-                remainder = remainder/primeNumber;
-            }
-            console.log("prime " + primeNumber + " exponent is " + exponent);
-            console.log("remainder", remainder);
-            exponents.push(exponent);
-            storedResult = remainder;
-        }
-        else {
-            remainder = storedResult;
-        }
-        //// CHECK IF WE'RE DONE
-        if ( remainder <= 1) {
-            return exponents;
-            continue;
-        }
-    }
-    
-    return exponents;
-}
-
-function cleanDivision (number) {
-    var integer = parseInt(number);
-    if ( number == integer ) return true;
-
-    return false;
-}
-
-function countBoxFactors () {
-    for ( var boxIndex = 0; boxIndex < 1000; boxIndex++ ) {
-        var boxNumber = boxIndex + 1;
-//        console.log("boxNumber", boxNumber);
-        var exponents = boxPrimeExponents[boxIndex];
-        //console.log("box Number " + boxNumber + " exponents", exponents);
-        //console.log("DEBUG EXIT");
-        //return;
-        var factorCount = 1;
-        for ( var index = 0; index < exponents.length; index++ ){
-            var exponent = exponents[index];
-            //console.log("exponent " + (index + 1) + ": " + exponent);
-            factorCount = factorCount * (exponent + 1);
-        }
-        console.log("box Number " + boxNumber + " factorCount: " + factorCount);
-    }
-    
-}
-
-precalculatePrimes(1000);
-
-findBoxExponents();
-
-countBoxFactors();
-
-*/
